4fe77be0f3172451e735560c2891de29e79c49a3
Winnerson Kharsunai <winnerson.k@intimetec.com>
2020-09-29 23:09:53 +0530

    demo

---
+++ Marianhill/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    $config = [
+        'LIB_PATH' => APPLICATION_PATH . DS . 'lib' . DS,
+        'VIEW_PATH' => APPLICATION_PATH . DS . 'view' . DS,
+        'MODEL_PATH' => APPLICATION_PATH . DS . 'model' . DS,
+    ];
+
+    require $config['LIB_PATH'] . 'functions.php';
+?>
